b2e4285a55919e71ee9a65968a4789987fa10a20
Thyme-git <1715764663@qq.com>
2023-07-06 14:03:58 +0800

    regular update

---
+++ test.py
@@ -0,0 +1,64 @@
+import os
+import glob
+import filecmp
+import subprocess
+import itertools
+
+class TestProgram:
+    def __init__(self, program_path, test_folder, dump_path):
+        self.program_path = program_path
+        self.dump_path = dump_path
+        self.test_folder = test_folder
+        self.passed_tests = 0
+        self.failed_tests = 0
+
+    def get_test_cases(self):
+        # 获取所有的测试用例文件夹
+        return glob.glob(f'{self.test_folder}/*')
+
+    def run_test(self, test_case_folder):
+        # 创建一个子进程来运行游戏
+        with open(f'{test_case_folder}/input', 'r') as input_file:
+            process = subprocess.Popen([f"{self.program_path}","dump"], stdin=subprocess.PIPE, stdout=subprocess.PIPE, universal_newlines=True)
+            for line in input_file:
+                process.stdin.write(line)
+                process.stdin.flush()
+        process.communicate()
+
+        # 重命名和移动dump
+        os.rename(f'{self.dump_path}', f'{test_case_folder}/dump')
+        # 清除文件末尾的空行
+        with open(f'{test_case_folder}/dump', 'r') as dump_file:
+            lines = dump_file.readlines()
+        if lines:
+            lines[-1] = lines[-1].rstrip('\n')        
+        with open(f'{test_case_folder}/dump', 'w') as dump_file:
+            dump_file.writelines(lines)
+
+        # 比较输出文件和预期输出文件
+        if filecmp.cmp(f'{test_case_folder}/output', f'{test_case_folder}/dump'):
+            print('\033[32m' + f':) Test "{test_case_folder}" passed!' + '\033[0m')
+            self.passed_tests += 1
+        else:
+            print('\033[31m' + f':( Test "{test_case_folder}" failed!' + '\033[0m')
+            with open(f'{test_case_folder}/output', 'r') as expected_file, open(f'{test_case_folder}/dump', 'r') as actual_file:
+
+                for i, (expected_line, actual_line) in enumerate(itertools.zip_longest(expected_file, actual_file), start=1):
+                    expected_line = expected_line.rstrip() if expected_line is not None else "None"
+                    actual_line = actual_line.rstrip() if actual_line is not None else "None"
+                    if repr(expected_line) != repr(actual_line):
+                        print(f'    Line {i}: expected "{expected_line}", but got "{actual_line}"')
+                          
+            self.failed_tests += 1
+
+    def run_all_tests(self):
+        # 运行所有测试用例
+        for test_case_folder in self.get_test_cases():
+            self.run_test(test_case_folder)
+
+        print('\033[33m' + 'RESULT'.center(80, '=') + '\033[0m')
+        print('\033[32m' + f'Passed tests: ({self.passed_tests}/{self.failed_tests + self.passed_tests})' + '\033[0m')
+
+if __name__ == '__main__':
+    tester = TestProgram('rich-game/main', 'test1', 'dump')#改test为你的文件夹名称，会自动遍历里面的小文件夹
+    tester.run_all_tests()
---
+++ test1/upgrade/output_upgrade.md
@@ -0,0 +1,51 @@
+user QASJ
+preuser Q
+Q
+alive 1
+money 1100
+point 0
+item1 0
+item2 0
+item3 0
+buff 0
+stop 0
+userloc 3
+A
+alive 1
+money 1000
+point 0
+item1 0
+item2 0
+item3 0
+buff 0
+stop 0
+userloc 30
+S
+alive 1
+money 1200
+point 0
+item1 0
+item2 0
+item3 0
+buff 0
+stop 0
+userloc 45
+J
+alive 1
+money 1500
+point 0
+item1 0
+item2 0
+item3 0
+buff 0
+stop 0
+userloc 1
+MAP
+mapuser 1 J
+mapsuer 3 Q
+mapsuer 30 A
+mapuser 45 S
+map 1 J 3
+map 3 Q 2
+map 30 A 2
+map 45 S 3
